30c0f52ba748e9f5689b029f8233ff1c34f484ee
plutonska <wendimivo96@gmail.com>
2024-06-01 21:31:55 +0700

    feat: add CRUD functionality for users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('pages.auth.auth-login');
@@ -10,4 +12,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('home', function () {
         return view('pages.dashboard', ['type_menu' => 'home']);
     })->name('home');
+
+    Route::resource('users', UserController::class);
 });
